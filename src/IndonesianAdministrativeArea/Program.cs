8d9afe43d8b324dc95908389e6205b06a2ec1e21
Taufan Augusta <taufan.augusta@protonmail.com>
2025-07-21 19:20:49 +0700

    update `AdministrativeAreaService`

--- src/IndonesianAdministrativeArea/Program.cs
+++ src/IndonesianAdministrativeArea/Program.cs
@@ -14,11 +14,14 @@ internal class Program
 
         List<ProvinceDto> provinceDtos = await AdministrativeAreaService.GetIndonesianProvinces();
         List<RegencyDto> regencyDtos = await AdministrativeAreaService.GetIndonesianRegencies(provinceDtos);
+        List<DistrictDto> districtDtos = await AdministrativeAreaService.GetIndonesianDistrict(regencyDtos);
+        List<VillageDto> villageDtos = await AdministrativeAreaService.GetIndonesianVillages(districtDtos);
 
         Console.WriteLine("Complete.\n");
 
         JsonService.Serializer.SerializeToJson<ProvinceDto>(provinceDtos, "provinces.json");
         JsonService.Serializer.SerializeToJson<RegencyDto>(regencyDtos, "regencies.json");
-
+        JsonService.Serializer.SerializeToJson<DistrictDto>(districtDtos, "districts.json");
+        JsonService.Serializer.SerializeToJson<VillageDto>(villageDtos, "villages.json");
     }
 }
--- src/IndonesianAdministrativeArea/Services/AdministrativeAreaService.cs
+++ src/IndonesianAdministrativeArea/Services/AdministrativeAreaService.cs
@@ -41,4 +41,56 @@ public static class AdministrativeAreaService
 
         return indonesianRegencies;
     }
+
+    public static async Task<List<DistrictDto>> GetIndonesianDistrict(List<RegencyDto> regencyDtos)
+    {
+        int numberOfRegencies = regencyDtos.Count;
+        int progress = 0;
+
+        List<DistrictDto> indonesianDistricts = [];
+
+        foreach (var regency in regencyDtos)
+        {
+            string url = $"https://wilayah.id/api/districts/{regency.Code}.json";
+            WilayahIdResponse? response = await HttpClientService.GetAdministrativeArea(url)
+                ?? throw new NullReferenceException();
+
+            List<DistrictDto> dtos = JsonService.Deserializer.DeserializeDistrictDtos(response);
+            indonesianDistricts.AddRange(dtos);
+
+            progress++;
+            double percent = (double)progress / numberOfRegencies * 100;
+            Console.Write($"\rDistricts: {percent:F0}% ({progress}/{numberOfRegencies})");
+        }
+
+        Console.Write("\n");
+
+        return indonesianDistricts;
+    }
+
+    public static async Task<List<VillageDto>> GetIndonesianVillages(List<DistrictDto> districtDtos)
+    {
+        int numberOfDistricts = districtDtos.Count;
+        int progress = 0;
+
+        List<VillageDto> indonesianVillage = [];
+
+        foreach (var district in districtDtos)
+        {
+            string url = $"https://wilayah.id/api/villages/{district.Code}.json";
+            WilayahIdResponse? response = await HttpClientService.GetAdministrativeArea(url)
+                ?? throw new NullReferenceException();
+
+            List<VillageDto> dtos = JsonService.Deserializer.DeserializeVillageDtos(response);
+            indonesianVillage.AddRange(dtos);
+
+            progress++;
+            double percent = (double)progress / numberOfDistricts * 100;
+            Console.Write($"\rVillages: {percent:F0}% ({progress}/{numberOfDistricts})");
+        }
+
+        Console.Write("\n");
+
+        return indonesianVillage;
+    }
 }
