31237e1334332a0b8bc33aca620a10732b73dba4
Taufan Augusta <taufan.augusta@protonmail.com>
2025-07-22 12:41:34 +0700

    add `IndexService`

---
+++ src/IndonesianAdministrativeArea/Services/IndexService.cs
@@ -0,0 +1,32 @@
+using System.Text.Json;
+
+namespace IndonesianAdministrativeArea.Services;
+
+public static class IndexService
+{
+        public static void SerializeIndexJson<T>(List<T> administrativeArea, string fileName = "index.json")
+        {
+            string json = JsonSerializer.Serialize(administrativeArea, GetOptions());
+
+            WriteIndexJson(fileName, json);
+        }
+
+        private static void WriteIndexJson(string fileName, string jsonBody)
+    {
+        string projectDir = Directory.GetCurrentDirectory();
+        string filePath = Path.Combine(projectDir, "..", "..", "data", "index", fileName);
+        string fullPath = Path.GetFullPath(filePath);
+
+        Directory.CreateDirectory(Path.GetDirectoryName(fullPath)!);
+
+        File.WriteAllText(fullPath, jsonBody);
+    }
+
+        private static JsonSerializerOptions GetOptions()
+        {
+            return new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+        }
+}
